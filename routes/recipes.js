f35e54f05ceb0baaf6a29c10f15a6ef6c3102cac
Zach Weiss <zweiss1@trinity.edu>
2025-03-07 23:46:54 -0600

    You can add recipes now

--- routes/recipes.js
+++ routes/recipes.js
@@ -37,7 +37,59 @@ router.get('/addrecipe', (req, res) => {
 router.post("/addfood", (req, res) => {
   console.log(req.body);
 
-  let sql = "sqlcommandhere"
+  //get all ingredients from the request
+  const ingredients = [];
+  Object.keys(req.body).forEach((key) => {
+    console.log(key);
+    if (key != 'name' && key != 'description' && key != 'steps'){
+      //at this point, the key is either an ingredient name or 'mainprotein' mapping to another ingredient
+      if (key == 'mainprotein') ingredients.push(req.body[key]);
+      else ingredients.push(key);
+    }
+  });
+  console.log(ingredients);
+
+  // insert the new recipe into the recipe table
+  let recipeQuery = `INSERT INTO recipes (name, description, instructions, protein) VALUES ("${req.body.name}", "${req.body.description}", "${req.body.steps}", "${req.body.mainprotein}");`
+  
+  //get a list of every recipe-ingredient pair we're adding to the junction table
+  let junctions = ingredients.map((ingredient) => [req.body.name, ingredient]);
+  let junctionQuery = `INSERT INTO junction (recipe_name, ingredient_name) VALUES ?`;
+
+  // According to AI, wrapping multiple queries in a transaction lets you roll back all of them if one fails
+  database.beginTransaction((err) => {
+    if (err) throw err;
+    console.log("Transaction initiated!");
+    //send the recipe to the recipe table
+    database.query(recipeQuery, (err) => {
+      if (err){
+        //roll back all changes from this transaction if there is an error
+        return database.rollback(() => {
+          throw err;
+        });
+      }
+      console.log("Recipe query successful!");
+      //connect the recipe to its ingredients
+      database.query(junctionQuery, [junctions], (err) => {
+        if (err){
+          return database.rollback(() => {
+            throw err;
+          });
+        }
+        console.log("All queries successful!");
+
+        database.commit((err) => {
+          if (err){
+            return database.rollback(() => {
+              throw err;
+            });
+          }
+          console.log("Transaction complete!");
+        });
+      });
+
+    })
+  });
 });
 
 //test DB connection
